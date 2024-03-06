aabd99d8052c346b9dadb4b23972b70e61db5af7
yuri <yuriducu04@gmail.com>
2024-03-06 09:11:51 -0500

    formulario terminado yuri

--- crear_usu.php
+++ crear_usu.php
@@ -1,34 +1,31 @@
 <?php
-	require 'conexion/database.php';
+    require 'conexion/database.php';
     $db = new Database();
     $con = $db->conectar();
 
 ?>
 
-
 <!--  -->
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Validación de Formulario con Javascript</title>
-	<link rel="stylesheet" href="https://necolas.github.io/normalize.css/8.0.1/normalize.css">
-	<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet"> 
-	<link rel="stylesheet" href="css/css.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Validación de Formulario con Javascript</title>
+    <link rel="stylesheet" href="https://necolas.github.io/normalize.css/8.0.1/normalize.css">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet"> 
+    <link rel="stylesheet" href="css/css.css">
 </head>
 
 <body>
    <main>
         <form  method="POST" autocomplete="off" class="formulario" id="formulario">
             
-
                 <!-- div para capturar el documento -->
 
                 <div class="formulario__grupo-input" id="grupo__usuario">
                     <label for="usuario" class="formulario__label">Documento *</label>
-                        <div class="formulario__checkbox">
+                        <div class="formulario__grupo-input">
                             <input type="text" class="formulario__input" name="usuario" id="usuario" placeholder="Documento">
                             <i class="formulario__validacion-estado fas fa-times-circle"></i>
                         </div>
@@ -40,18 +37,18 @@
 
                 <div class="formulario__grupo-input" id="grupo__nombre">
                     <label for="nombre" class="formulario__label">Nombres *</label>
-                        <div class="">
+                        <div class="formulario__grupo-input">
                             <input type="text" class="formulario__input" onkeyup="mayus(this);" name="nombre" id="nombre" placeholder="Nombres">
-                            <i class=""></i>
+                            <i class="formulario__validacion-estado fas fa-times-circle"></i>
                         </div>
                         <p class="formulario__input-error">
                             El usuario tiene que ser de 12 a 40 dígitos y solo puede contener letras</p>
                 </div>
-
+                
                 <!-- Grupo: Contraseña -->
-                <div class="formulario__grupo-input">
+                <div class="formulario__grupo-input"  id="grupo__password">
                     <label for="password" class="formulario__label">Contraseña *</label>
-                    <div class="">
+                    <div class="formulario__grupo-input">
                         <input  onkeyup="minus(this);" type="password" class="formulario__input" name="password" id="password">
                         <i class="formulario__validacion-estado fas fa-times-circle"></i>
                     </div>
@@ -61,27 +58,26 @@
                 <!-- Grupo: Contraseña 2 -->
                 <div class="formulario__grupo-input" id="grupo__password2">
                     <label for="password2" class="formulario__label">Repetir Contraseña *</label>
+                    <div class="formulario__grupo-input">
                         <input type="password" class="formulario__input" name="password2" id="password2">
                         <i class="formulario__validacion-estado fas fa-times-circle"></i>
                     </div>
                     <p class="formulario__input-error">Ambas contraseñas deben ser iguales.</p>
                 </div>
-        
-
                 <!-- Grupo: Correo Electronico -->
                 <div class="formulario__grupo-input" id="grupo__correo">
                     <label for="correo" class="formulario__label">Correo Electrónico *</label>
-                    <div class="">
+                    <div class="formulario__grupo-input">
                         <input onkeyup="minus(this);" type="email" class="formulario__input" name="correo" id="correo" placeholder="correo@correo.com">
                         <i class="formulario__validacion-estado fas fa-times-circle"></i>
                     </div>
                     <p class="formulario__input-error">El correo solo puede contener letras, numeros, puntos, guiones y guion bajo.</p>
                 </div>
 
-                <div class="formulario__grupo-select" id="grupo__telefono">
+                <div class="formulario__grupo-input" id="grupo__telefono">
                     <label for="id_tip_use" class="formulario__label">Tipo Usuario *</label>
-				    <div class="">                 
-                        <select  name="id_tip_use" id="id_tip_use" class="formulario__input" required>
+                    <div class="formulario__grupo-select">               
+                        <select  name="id_tip_use" id="id_tip_use" class="formulario__select  " required>
                             <!-- <option value="" selected="">** Seleccione Tipo Usuario **</option> -->
                                 <?php
                                    /*Consulta para mostrar las opciones en el select */
@@ -98,22 +94,22 @@
 
                 
                 <!-- Grupo: Terminos y Condiciones -->
-			<div class="formulario__grupo-terminos" id="grupo__terminos">
-				<label class="formulario__label">
-					<input class="formulario__checkbox" type="checkbox" name="terminos" id="terminos">
-					Acepto los Terminos y Condiciones
-				</label>
-			</div>
+            <div class="formulario__checkbox" id="grupo__terminos">
+                <label class="formulario__checkbox">
+                    <input class="formulario__checkbox" type="checkbox" name="terminos" id="terminos">
+                    Acepto los Terminos y Condiciones
+                </label>
+            </div>
 
-			<div class="formulario__mensaje" id="formulario__mensaje">
-				<p><i class="fas fa-exclamation-triangle"></i> <b>Error:</b> Por favor rellena el formulario correctamente. </p>
-			</div>
+            <div class="formulario__mensaje" id="formulario__mensaje">
+                <p><i class="fas fa-exclamation-triangle"></i> <b>Error:</b> Por favor rellena el formulario correctamente. </p>
+            </div>
             
             <p class="text-center">
                       
             <div class="formulario__grupo-btn-enviar">
                 <button type="submit" class="formulario__btn" name="save" value="guardar" >Enviar</button>
-                <p class="formulario__mensaje-exito" id="formulario__mensaje-exito">Formulario enviado exitosamente!</p>
+                <p class="formulario__mensaje" id="formulario__mensaje-exito">Formulario enviado exitosamente!</p>
             </div>
                 
         
@@ -121,7 +117,7 @@
    </main>
    <script src="js/jquery.js"></script>
    <script src="js/formulario.js"></script>
-	<script src="https://kit.fontawesome.com/2c36e9b7b1.js" crossorigin="anonymous"></script>
+    <script src="https://kit.fontawesome.com/2c36e9b7b1.js" crossorigin="anonymous"></script>
 
     <!--  Javascript funcion para convertor en mayusculas y minusculas -->
     <!-- <script src="../js/main.js"></script> -->
