2c96abe1186c4ea65fde9285c20a1ae43875998f
BRIAN_AVILA <160604984+brian-j-avila@users.noreply.github.com>
2024-03-06 06:29:53 -0500

    creo la carpeta con la copia de los archivos del main, para la correcta verificacion y realización de los archivos

---
+++ BRIAN_JULIAN_AVILA/crear_usu.php
@@ -0,0 +1,140 @@
+<?php
+	require 'conexion/database.php';
+    $db = new Database();
+    $con = $db->conectar();
+
+?>
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Validación de Formulario con Javascript</title>
+	<link rel="stylesheet" href="https://necolas.github.io/normalize.css/8.0.1/normalize.css">
+	<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet"> 
+	<link rel="stylesheet" href="css/css.css">
+</head>
+
+<body>
+   <main>
+        <form  method="POST" autocomplete="off" class="" id="formulario">
+            
+
+                <!-- div para capturar el documento -->
+
+                <div class="" id="grupo__usuario">
+                    <label for="usuario" class="">Documento *</label>
+                        <div class="">
+                            <input type="text" class="" name="usuario" id="usuario" placeholder="Documento">
+                            <i class="formulario__validacion-estado fas fa-times-circle"></i>
+                        </div>
+                        <p class="">
+                            El documento tiene que ser de 6 a 11 dígitos y solo puede contener numeros.</p>
+                </div>
+
+                <!-- div para capturar el nombre -->
+
+                <div class="" id="grupo__nombre">
+                    <label for="nombre" class="">Nombres *</label>
+                        <div class="">
+                            <input type="text" class="" onkeyup="mayus(this);" name="nombre" id="nombre" placeholder="Nombres">
+                            <i class=""></i>
+                        </div>
+                        <p class="">
+                            El usuario tiene que ser de 12 a 40 dígitos y solo puede contener letras</p>
+                </div>
+
+                <!-- Grupo: Contraseña -->
+                <div class="">
+                    <label for="password" class="">Contraseña *</label>
+                    <div class="">
+                        <input  onkeyup="minus(this);" type="password" class="" name="password" id="password">
+                        <i class="formulario__validacion-estado fas fa-times-circle"></i>
+                    </div>
+                    <p class="formulario__input-error">La contraseña tiene que ser de 8 a 12 dígitos Alfanumericos.</p>
+                </div>
+
+                <!-- Grupo: Contraseña 2 -->
+                <div class="" id="grupo__password2">
+                    <label for="password2" class="">Repetir Contraseña *</label>
+                        <input type="password" class="" name="password2" id="password2">
+                        <i class="formulario__validacion-estado fas fa-times-circle"></i>
+                    </div>
+                    <p class="">Ambas contraseñas deben ser iguales.</p>
+                </div>
+        
+
+                <!-- Grupo: Correo Electronico -->
+                <div class="" id="grupo__correo">
+                    <label for="correo" class="">Correo Electrónico *</label>
+                    <div class=">
+                        <input onkeyup="minus(this);" type="email" class="formulario__input" name="correo" id="correo" placeholder="correo@correo.com">
+                        <i class="formulario__validacion-estado fas fa-times-circle"></i>
+                    </div>
+                    <p class="">El correo solo puede contener letras, numeros, puntos, guiones y guion bajo.</p>
+                </div>
+
+                <div class="" id="grupo__telefono">
+                    <label for="id_tip_use" class="">Tipo Usuario *</label>
+				    <div class="">                 
+                        <select  name="id_tip_use" id="id_tip_use" class="" required>
+                            <!-- <option value="" selected="">** Seleccione Tipo Usuario **</option> -->
+                                <?php
+                                   /*Consulta para mostrar las opciones en el select */
+                                    $statement = $con->prepare('SELECT * from tip_use WHERE id_tip_use = 1');
+                                    $statement->execute();
+                                    while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
+                                      echo "<option value=" . $row['id_tip_use'] . ">" . $row['tip_use'] . "</option>";
+                                    }
+                                ?>
+                        </select>
+                    </div>
+                    
+                </div>  
+
+                
+                <!-- Grupo: Terminos y Condiciones -->
+			<div class="" id="grupo__terminos">
+				<label class="">
+					<input class="" type="checkbox" name="terminos" id="terminos">
+					Acepto los Terminos y Condiciones
+				</label>
+			</div>
+
+			<div class="" id="formulario__mensaje">
+				<p><i class="fas fa-exclamation-triangle"></i> <b>Error:</b> Por favor rellena el formulario correctamente. </p>
+			</div>
+            
+            <p class="text-center">
+                      
+            <div class="">
+                <button type="submit" class="" name="save" value="guardar" >Enviar</button>
+                <p class="" id="formulario__mensaje-exito">Formulario enviado exitosamente!</p>
+            </div>
+                
+        
+        </form>
+   </main>
+   <script src="js/jquery.js"></script>
+   <script src="js/formulario.js"></script>
+	<script src="https://kit.fontawesome.com/2c36e9b7b1.js" crossorigin="anonymous"></script>
+
+    <!--  Javascript funcion para convertor en mayusculas y minusculas -->
+    <!-- <script src="../js/main.js"></script> -->
+    <script>
+        function mayus(e) {
+        e.value = e.value.toUpperCase();
+        }
+
+        function minus(e) {
+        e.value = e.value.toLowerCase();
+        }
+    </script>
+  
+</body>
+
+</html>
