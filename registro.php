238ed9680e6122d0de4faeb6b7dd6ff6a569ffa6
kenner rojas <kenner181@gmail.com>
2024-03-06 11:25:48 -0500

    prueba push

--- registro.php
+++ registro.php
@@ -16,9 +16,16 @@
     echo $pas;
     echo $email;
     echo $usutip_usu;
-    
-
-
-
-
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Kenner Calderon Rojas</h1>
+</body>
+</html>
