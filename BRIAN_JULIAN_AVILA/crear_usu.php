199c182983d8faab5bc5c056d72d8ac0aa59de0f
BRIAN_AVILA <160604984+brian-j-avila@users.noreply.github.com>
2024-03-06 08:02:37 -0500

    Realizo los cambios respectivos y nombraqmientos de id y las clases respectivas en el formulario html

--- BRIAN_JULIAN_AVILA/crear_usu.php
+++ BRIAN_JULIAN_AVILA/crear_usu.php
@@ -1,7 +1,7 @@
 <?php
-	require 'conexion/database.php';
-    $db = new Database();
-    $con = $db->conectar();
+require 'conexion/database.php';
+$db = new Database();
+$con = $db->conectar();
 
 ?>
 
@@ -10,131 +10,137 @@
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Validación de Formulario con Javascript</title>
-	<link rel="stylesheet" href="https://necolas.github.io/normalize.css/8.0.1/normalize.css">
-	<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet"> 
-	<link rel="stylesheet" href="css/css.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Validación de Formulario con Javascript</title>
+    <link rel="stylesheet" href="https://necolas.github.io/normalize.css/8.0.1/normalize.css">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/css.css">
 </head>
 
 <body>
-   <main>
-        <form  method="POST" autocomplete="off" class="" id="formulario">
-            
+    <main>
+        <form method="POST" autocomplete="off" class="formulario" id="formulario">
 
-                <!-- div para capturar el documento -->
 
-                <div class="" id="grupo__usuario">
-                    <label for="usuario" class="">Documento *</label>
-                        <div class="">
-                            <input type="text" class="" name="usuario" id="usuario" placeholder="Documento">
-                            <i class="formulario__validacion-estado fas fa-times-circle"></i>
-                        </div>
-                        <p class="">
-                            El documento tiene que ser de 6 a 11 dígitos y solo puede contener numeros.</p>
+            <!-- div para capturar el documento -->
+
+            <div class="formulario__label" id="grupo__usuario">
+                <label for="usuario" class="formulario__label">Documento *</label>
+                <div class="formulario__grupo-input">
+                    <input type="text" class="formulario__input" name="usuario" id="usuario" placeholder="Documento">
+                    <i class="formulario__validacion-estado fas fa-times-circle"></i>
                 </div>
-
-                <!-- div para capturar el nombre -->
-
-                <div class="" id="grupo__nombre">
-                    <label for="nombre" class="">Nombres *</label>
-                        <div class="">
-                            <input type="text" class="" onkeyup="mayus(this);" name="nombre" id="nombre" placeholder="Nombres">
-                            <i class=""></i>
-                        </div>
-                        <p class="">
-                            El usuario tiene que ser de 12 a 40 dígitos y solo puede contener letras</p>
-                </div>
-
-                <!-- Grupo: Contraseña -->
-                <div class="">
-                    <label for="password" class="">Contraseña *</label>
-                    <div class="">
-                        <input  onkeyup="minus(this);" type="password" class="" name="password" id="password">
-                        <i class="formulario__validacion-estado fas fa-times-circle"></i>
-                    </div>
-                    <p class="formulario__input-error">La contraseña tiene que ser de 8 a 12 dígitos Alfanumericos.</p>
-                </div>
-
-                <!-- Grupo: Contraseña 2 -->
-                <div class="" id="grupo__password2">
-                    <label for="password2" class="">Repetir Contraseña *</label>
-                        <input type="password" class="" name="password2" id="password2">
-                        <i class="formulario__validacion-estado fas fa-times-circle"></i>
-                    </div>
-                    <p class="">Ambas contraseñas deben ser iguales.</p>
-                </div>
-        
-
-                <!-- Grupo: Correo Electronico -->
-                <div class="" id="grupo__correo">
-                    <label for="correo" class="">Correo Electrónico *</label>
-                    <div class=">
-                        <input onkeyup="minus(this);" type="email" class="formulario__input" name="correo" id="correo" placeholder="correo@correo.com">
-                        <i class="formulario__validacion-estado fas fa-times-circle"></i>
-                    </div>
-                    <p class="">El correo solo puede contener letras, numeros, puntos, guiones y guion bajo.</p>
-                </div>
-
-                <div class="" id="grupo__telefono">
-                    <label for="id_tip_use" class="">Tipo Usuario *</label>
-				    <div class="">                 
-                        <select  name="id_tip_use" id="id_tip_use" class="" required>
-                            <!-- <option value="" selected="">** Seleccione Tipo Usuario **</option> -->
-                                <?php
-                                   /*Consulta para mostrar las opciones en el select */
-                                    $statement = $con->prepare('SELECT * from tip_use WHERE id_tip_use = 1');
-                                    $statement->execute();
-                                    while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
-                                      echo "<option value=" . $row['id_tip_use'] . ">" . $row['tip_use'] . "</option>";
-                                    }
-                                ?>
-                        </select>
-                    </div>
-                    
-                </div>  
-
-                
-                <!-- Grupo: Terminos y Condiciones -->
-			<div class="" id="grupo__terminos">
-				<label class="">
-					<input class="" type="checkbox" name="terminos" id="terminos">
-					Acepto los Terminos y Condiciones
-				</label>
-			</div>
-
-			<div class="" id="formulario__mensaje">
-				<p><i class="fas fa-exclamation-triangle"></i> <b>Error:</b> Por favor rellena el formulario correctamente. </p>
-			</div>
-            
-            <p class="text-center">
-                      
-            <div class="">
-                <button type="submit" class="" name="save" value="guardar" >Enviar</button>
-                <p class="" id="formulario__mensaje-exito">Formulario enviado exitosamente!</p>
+                <p class="formulario__input-error">
+                    El documento tiene que ser de 6 a 11 dígitos y solo puede contener numeros.</p>
             </div>
-                
-        
+
+            <!-- div para capturar el nombre -->
+
+            <div class="formulario__label" id="grupo__nombre">
+                <label for="nombre" class="formulario__label">Nombres *</label>
+                <div class="formulario__grupo-input">
+                    <input type="text" class="formulario__input" onkeyup="mayus(this);" name="nombre" id="nombre" placeholder="Nombres">
+                    <i class="formulario__validacion-estado fas fa-times-circle"></i>
+                </div>
+                <p class="formulario__input-error">El usuario tiene que ser de 12 a 40 dígitos y solo puede contener letras</p>
+            </div>
+
+            <!-- Grupo: Contraseña -->
+            <div class="formulario__label" id="grupo__password">
+                <label for="password" class="formulario__label">Contraseña *</label>
+                <div class="formulario__grupo-input" id="grupo__password">
+                    <input onkeyup="minus(this);" type="password" class="formulario__input" name="password" id="password">
+                    <i class="formulario__validacion-estado fas fa-times-circle"></i>
+                </div>
+                <p class="formulario__input-error">La contraseña tiene que ser de 8 a 12 dígitos Alfanumericos.</p>
+
+            </div>
+
+            <!-- Grupo: Contraseña 2 -->
+
+            <div class="formulario__label" id="grupo__password2">
+                <label for="password2" class="formulario__label">Repetir Contraseña *</label>
+                <div class="formulario__grupo-input" id="grupo__password2">
+                    <i class="formulario__validacion-estado fas fa-times-circle"></i>
+                    <input type="password" class="formulario__input" name="password2" id="password2">
+
+                </div>
+                <p class="formulario__input-error">Ambas contraseñas deben ser iguales.</p>
+            </div>
+
+
+
+
+            <!-- Grupo: Correo Electronico -->
+            <div class="formulario__label" id="grupo__correo">
+                <label for="correo" class="formulario__label">Correo Electrónico *</label>
+                <div class="formulario__grupo-input">
+                    <input onkeyup=" minus(this);" type="email" class="formulario__input" name="correo" id="correo" placeholder="correo@correo.com">
+                    <i class="formulario__validacion-estado fas fa-times-circle"></i>
+                </div>
+                <p class="formulario__input-error">El correo solo puede contener letras, numeros, puntos, guiones y guion bajo.</p>
+            </div>
+
+            <div class="formulario__label" id="grupo__telefono">
+                <label for="id_tip_use" class="formulario__label">Tipo Usuario *</label>
+                <div class="formulario__grupo-select">
+                    <select name="id_tip_use" id="id_tip_use" class="formulario__select " required>
+                        <!-- <option value="" selected="">** Seleccione Tipo Usuario **</option> -->
+                        <?php
+                        /*Consulta para mostrar las opciones en el select */
+                        $statement = $con->prepare('SELECT * from tip_use WHERE id_tip_use = 1');
+                        $statement->execute();
+                        while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
+                            echo "<option value=" . $row['id_tip_use'] . ">" . $row['tip_use'] . "</option>";
+                        }
+                        ?>
+                    </select>
+                </div>
+
+            </div>
+
+
+            <!-- Grupo: Terminos y Condiciones -->
+            <div class="formulario__grupo-terminos" id="grupo__terminos">
+                <label class="formulario__label">
+                    <input class="formulario__checkbox" type="checkbox" name="terminos" id="terminos">
+                    Acepto los Terminos y Condiciones
+                </label>
+            </div>
+
+            <div class="formulario__mensaje" id="formulario__mensaje">
+                <p><i class="fas fa-exclamation-triangle"></i> <b>Error:</b> Por favor rellena el formulario correctamente. </p>
+            </div>
+
+            <p class="text-center">
+
+            <div class="formulario__grupo-btn-enviar">
+                <button type="submit" class="formulario__btn" name="save" value="guardar">Enviar</button>
+                <p class="formulario__mensaje-exito" id="formulario__mensaje-exito">Formulario enviado exitosamente!</p>
+            </div>
+
+
         </form>
-   </main>
-   <script src="js/jquery.js"></script>
-   <script src="js/formulario.js"></script>
-	<script src="https://kit.fontawesome.com/2c36e9b7b1.js" crossorigin="anonymous"></script>
+    </main>
+    <script src="js/jquery.js"></script>
+    <script src="js/formulario.js"></script>
+    <script src="https://kit.fontawesome.com/2c36e9b7b1.js" crossorigin="anonymous"></script>
 
     <!--  Javascript funcion para convertor en mayusculas y minusculas -->
     <!-- <script src="../js/main.js"></script> -->
     <script>
         function mayus(e) {
-        e.value = e.value.toUpperCase();
+            e.value = e.value.toUpperCase();
         }
 
         function minus(e) {
-        e.value = e.value.toLowerCase();
+            e.value = e.value.toLowerCase();
         }
     </script>
-  
+
 </body>
 
 </html>
