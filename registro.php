8096af40e5b55df9361802f8b167520276abf498
juandavid1033 <135982349+juandavid1033@users.noreply.github.com>
2024-03-10 16:58:55 -0500

    Update registro.php

--- registro.php
+++ registro.php
@@ -6,7 +6,7 @@ $con = $db->conectar();
 $usu = $_POST['doc'];
 $nom = $_POST['nom'];
 $colorfav = $_POST['colorfav'];
-$cel = $_POST['cel'];
+$celular_fijo = $_POST['celular_fijo'];
 $pas = $_POST['pas'];
 $email = $_POST['email'];
 $tip_usu = $_POST['tip_usu'];
@@ -14,14 +14,14 @@ $tip_usu = $_POST['tip_usu'];
 echo $usu;
 echo $nom;
 echo $colorfav;
-echo $cel;
+echo $celular_fijo;
 echo $pas;
 echo $email;
 echo $tip_usu;
 
 
 $insertsql = $con->prepare("INSERT INTO user (doc, name, colorfav, celular_fijo, contrasena, email, id_tip_user, estado) VALUES (?, ?, ?, ?, ?, ?, ?, ?)");
-$filaa1 = $insertsql->execute([ $usu, $nom, $colorfav, $cel, $pas, $email, $tip_usu, 1]);
+$filaa1 = $insertsql->execute([ $usu, $nom, $colorfav, $celular_fijo, $pas, $email, $tip_usu, 1]);
 
 if ($filaa1) {
     echo '<script>alert("Datos registrados exitosamente");</script>';
