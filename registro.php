296a16344bce64de6ca53c8750740cb4c4703261
jeferson <yiyecardenal@gmail.com>
2024-03-11 10:14:09 -0500

    recargue de un select

--- registro.php
+++ registro.php
@@ -3,6 +3,10 @@
     $db = new Database();
     $con = $db->conectar();
 
+// variable para confirmar envio
+
+$validacion=0;
+
 // Recibe los datos enviados por AJAX
 $doc = $_POST['doc'];
 $nombre = $_POST['nom'];
@@ -12,24 +16,32 @@ $contra = $_POST['pas'];
 $correo = $_POST['email'];
 $tip_user = $_POST['tip_usu'];
 
-echo $doc;
-echo $nombre;
-echo $dir;
-echo $edad;
-echo $contra;
-echo $correo;
-echo $tip_user;
+// echo $doc;
+// echo $nombre;
+// echo $dir;
+// echo $edad;
+// echo $contra;
+// echo $correo;
+// echo $tip_user;
 
 // Prepara la consulta SQL para insertar los datos en la base de datos
-    if {
 
-        $cifrado = password_hash($contra,PASSWORD_DEFAULT, array("pass"=>12));
-        
-        $insertSQL = $con->prepare ("INSERT INTO user (doc,name,direccion,edad,contrasena,email,id_tip_user) VALUES ('$doc', '$nombre','$dir', '$edad','$cifrado', '$correo','$tip_user')");
-        $insertSQL -> execute();
-        echo '<script> alert("REGISTRO EXITOSO");</script>';
-     }  
+// Prepara la consulta SQL para insertar los datos en la base de datos
+$sql= $con -> prepare ("SELECT * FROM user WHERE doc='$doc'");
+$sql -> execute();
+$fila = $sql -> fetchAll(PDO::FETCH_ASSOC);
 
+if ($fila){
+   echo '<script>alert ("ESTE PAQUETE YA EXISTE //CAMBIELO//");</script>';
+   $validacion=0;
+}
+else {
+   $cifrado = password_hash($contra,PASSWORD_DEFAULT, array("pass"=>12));
+   
+   $insertSQL = $con->prepare ("INSERT INTO user (doc,name,direccion,edad,contrasena,email,id_tip_user) VALUES ('$doc', '$nombre','$dir', '$edad','$cifrado', '$correo','$tip_user')");
+   $insertSQL -> execute();
+   $validacion=1;
+}
+?>
 
-
-
+<script src="formulario.js"></script>
