abb252dc8bbda78fbfc4dc44b46386bfa8ed22ba
BRIAN_AVILA <160604984+brian-j-avila@users.noreply.github.com>
2024-03-11 11:03:42 -0500

    mudificacion en el archivo deportes2.php

--- BRIAN_JULIAN_AVILA/deportes.php
+++ BRIAN_JULIAN_AVILA/deportes.php
@@ -14,7 +14,7 @@ $con = $db->conectar();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Validación de Formulario con Javascript</title>
+    <title>Validación de Formulario con Javascriptss</title>
     <link rel="stylesheet" href="https://necolas.github.io/normalize.css/8.0.1/normalize.css">
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="css/css.css">
